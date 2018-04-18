ae144dfed3edb7fd37aeecd74a35e1cc4551c305
xiongchenyu6 <xiongchenyu6@gmail.com>
2018-04-18 21:59:51 +0800

    fixes

--- Report/auto/aireport3.el
+++ Report/auto/aireport3.el
@@ -5,12 +5,12 @@
                      '(("article" "titlepage")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,18 +31,18 @@
     "hyperref"
     "mathptmx")
    (LaTeX-add-labels
-    "sec:org1a1622c"
-    "tab:org01e1fd0"
-    "sec:orge78b22e"
-    "tab:org1266cfd"
-    "sec:org2848804"
-    "tab:org867b394"
-    "tab:org224342a"
-    "sec:org5fe8d9d"
-    "tab:org5961def"
-    "sec:org3db3d09"
-    "tab:org77e45c1"
-    "sec:orga03e7af"
-    "sec:org7c52b77"))
+    "sec:org2a59890"
+    "tab:org748bca0"
+    "sec:orgf5e4b31"
+    "tab:org76e6c75"
+    "sec:org757358b"
+    "tab:org27cd88f"
+    "tab:orgee0727c"
+    "sec:org3f87eaf"
+    "tab:org9811961"
+    "sec:org96c0763"
+    "tab:org631ea84"
+    "sec:orgb518588"
+    "sec:org7170524"))
  :latex)
 
---
+++ Report/auto/videoProcessingAssignment.el
@@ -0,0 +1,43 @@
+(TeX-add-style-hook
+ "videoProcessingAssignment"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "titlepage")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "mathptmx")
+   (LaTeX-add-labels
+    "sec:orgc723f27"
+    "sec:org80e6a1e"
+    "sec:org1d8ee8a"
+    "sec:orgade3232"
+    "sec:orgba33989"
+    "sec:orgece9221"
+    "sec:org33cff40"
+    "sec:org9ab7902"))
+ :latex)
+

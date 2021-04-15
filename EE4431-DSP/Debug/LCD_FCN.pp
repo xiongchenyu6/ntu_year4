3ba499ec011434f6d1d1d90a797f7af55e8f947d
xiongchenyu <xiongchenyu@bigo.sg>
2021-04-16 07:38:01 +0800

    add sg2 dest and set it location to ceto2

--- EE4431-DSP/Debug/LCD_FCN.pp
+++
@@ -1,6 +0,0 @@
-# FIXED
-LCD_FCN.obj: ../LCD_FCN.c
-LCD_FCN.obj: C:/EE4413\ Course\ Materials/Part\ 2\ of\ 2_By\ Prof\ Joni\ Lie/EE4413_C515/C5515_Support_Files/452_Support/usbstk5515.h
-LCD_FCN.obj: C:/EE4413\ Course\ Materials/Part\ 2\ of\ 2_By\ Prof\ Joni\ Lie/EE4413_C515/C5515_Support_Files/452_Support/usbstk5515_i2c.h
-LCD_FCN.obj: C:/EE4413\ Course\ Materials/Part\ 2\ of\ 2_By\ Prof\ Joni\ Lie/EE4413_C515/C5515_Support_Files/452_Support/usbstk5515_gpio.h
-LCD_FCN.obj: ../font.h
--- EE4431-DSP/Debug/led.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-led.obj: ../led.c
-led.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/stdio.h
-led.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/linkage.h
-led.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/stdarg.h
-led.obj: C:/EE4413\ Course\ Materials/Part\ 2\ of\ 2_By\ Prof\ Joni\ Lie/EE4413_C515/C5515_Support_Files/452_Support/usbstk5515.h
-led.obj: ../led.h
